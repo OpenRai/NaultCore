7028e21118ff57cd7c5f3cc55f9738f85a51cc95
Conny Brunnkvist <cbrunnkvist@gmail.com>
2026-09-01 00:09:15 +0700

    Migrate sweeper behavior coverage to Vitest

--- src/app/components/sweeper/sweeper.component.spec.ts
+++ src/app/components/sweeper/sweeper.component.spec.ts
@@ -1,4 +1,6 @@
 import { ElementRef } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { TestBed } from '@angular/core/testing';
 import { BehaviorSubject } from 'rxjs';
 import * as nanocurrency from 'nanocurrency';
@@ -13,15 +15,25 @@ import { AppSettingsService } from '../../services/app-settings.service';
 import { NanoBlockService } from '../../services/nano-block.service';
 import { UtilService } from '../../services/util.service';
 import { Router } from '@angular/router';
+import { RaiPipe } from '../../pipes/rai.pipe';
+import { SqueezePipe } from '../../pipes/squeeze.pipe';
+import { AmountSplitPipe } from '../../pipes/amount-split.pipe';
+
+@NgModule({
+  declarations: [SweeperComponent, RaiPipe, SqueezePipe, AmountSplitPipe],
+  imports: [FormsModule],
+})
+class SweeperTestModule {}
 
 describe('SweeperComponent', () => {
-  let notification: jasmine.SpyObj<NotificationService>;
-  let api: jasmine.SpyObj<ApiService>;
+  let notification: { sendInfo: ReturnType<typeof vi.fn> };
+  let api: { accountInfo: ReturnType<typeof vi.fn> };
 
   beforeEach(() => {
-    notification = jasmine.createSpyObj<NotificationService>('NotificationService', ['sendInfo']);
-    api = jasmine.createSpyObj<ApiService>('ApiService', ['accountInfo']);
+    notification = { sendInfo: vi.fn() };
+    api = { accountInfo: vi.fn() };
     TestBed.configureTestingModule({
+      imports: [SweeperTestModule],
       providers: [
         { provide: WalletService, useValue: {
           walletState: { accounts: [], selectedAccount: null },
@@ -31,9 +43,9 @@ describe('SweeperComponent', () => {
         { provide: NotificationService, useValue: notification },
         { provide: ModalService, useValue: {} },
         { provide: ApiService, useValue: api },
-        { provide: WorkPoolService, useValue: jasmine.createSpyObj('WorkPoolService', ['getWork', 'suppressPrecomputation']) },
+        { provide: WorkPoolService, useValue: { getWork: vi.fn(), suppressPrecomputation: vi.fn() } },
         { provide: AppSettingsService, useValue: { settings: {} } },
-        { provide: NanoBlockService, useValue: jasmine.createSpyObj('NanoBlockService', ['getRandomRepresentative']) },
+        { provide: NanoBlockService, useValue: { getRandomRepresentative: vi.fn() } },
         { provide: UtilService, useValue: {} },
         { provide: Router, useValue: { getCurrentNavigation: () => ({ extras: { state: {} } }) } },
       ],
@@ -46,7 +58,7 @@ describe('SweeperComponent', () => {
     const destination = nanocurrency.deriveAddress(nanocurrency.derivePublicKey(privateKey), { useNanoPrefix: true });
     component.destinationAccount = destination;
     component.logArea = { nativeElement: { scrollTop: 0, scrollHeight: 0 } } as ElementRef;
-    const done = jasmine.createSpy('done');
+    const done = vi.fn();
 
     await component.processAccount(privateKey, done);
 
